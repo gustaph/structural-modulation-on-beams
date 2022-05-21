058db16bfa6779d2cc91b15d254fc651a0032ebb
Gustavo Soares <gustavosoares112@gmail.com>
2022-05-21 00:17:28 -0300

    plot unif. distributed load

--- plot_beam.py
+++ plot_beam.py
@@ -14,8 +14,10 @@ HATCH_FIXED = "//"
 HATCH_ROLLER = "OO"
 
 TEXT_SPACE = 0.06 * HEIGHT
-Y_DISTANCE = 0.35 * HEIGHT
+Y_DISTANCE = 0.25 * HEIGHT
 
+N_ARROWS_MULTIPLIER = 2
+ARROW_WIDTH = 0.4
 
 class Plot:
     def __init__(self, L: float, supports: List[Support], loads: List[Load]):
@@ -25,8 +27,7 @@ class Plot:
 
     def plot(self):
         fig, ax = plt.subplots(figsize=(15, 6))
-        ax.plot(range(np.floor(self.L).astype(int)),
-                [0] * np.floor(self.L).astype(int), color="black", linewidth=5)
+        ax.plot([0, self.L], [0, 0], color="black", linewidth=5)
 
         support_patches = self._plot_supports(ax)
         load_patches = self._plot_loads(ax)
@@ -51,10 +52,10 @@ class Plot:
                     alpha = -WIDTH/2
 
                 patch = patches.Rectangle((support.position + alpha, -HEIGHT/2),
-                                      width=WIDTH,
-                                      height=HEIGHT,
-                                      hatch=HATCH_FIXED,
-                                      fill=False)
+                                          width=WIDTH,
+                                          height=HEIGHT,
+                                          hatch=HATCH_FIXED,
+                                          fill=False)
                 patch_elements.append(patch)
 
         return patch_elements
@@ -62,17 +63,31 @@ class Plot:
     def _plot_loads(self, ax):
         patch_elements = []
         for load in self.loads:
+            text = f"{load.magnitude}N"
+
             if load.category == LoadTypes.centered:
                 arrow = patches.Arrow(x=load.start, y=Y_DISTANCE,
-                                dx=0, dy=-Y_DISTANCE, color="red", width=0.6, clip_on=False)
-                text = f"{load.magnitude}N"
+                                      dx=0, dy=-Y_DISTANCE, color="red", width=ARROW_WIDTH)
+
                 ax.text(load.start, Y_DISTANCE + TEXT_SPACE, s=text, ha='center', va='top',
                         weight='normal', fontfamily='monospace', fontsize='large')
                 patch_elements.append(arrow)
-                
+
             elif load.category == LoadTypes.uniformlyDistributed:
-                pass
-            
+                distance = np.abs(load.end - load.start)
+                n_arrows = int(N_ARROWS_MULTIPLIER * distance)
+                arrow_positions = np.linspace(load.start, load.end, n_arrows)
+                
+                arrows = [patches.Arrow(x=position, y=Y_DISTANCE, dx=0, dy=-Y_DISTANCE, color="red", width=ARROW_WIDTH)
+                          for position in arrow_positions]
+
+                ax.text((distance / 2) + load.start, Y_DISTANCE + TEXT_SPACE, s=text, ha='center', va='top',
+                        weight='normal', fontfamily='monospace', fontsize='large')
+
+                ax.plot([load.start, load.end], [Y_DISTANCE, Y_DISTANCE], color="red", lw=3)
+
+                patch_elements.extend(arrows)
+
             elif load.category == LoadTypes.uniformlyVarying:
                 pass
 
@@ -80,8 +95,11 @@ class Plot:
 
 
 if __name__ == '__main__':
-    p = p = Plot(20, [Support(0.0, "fixed"), Support(20.0, "fixed")],
-                 [Load(15.0, LoadTypes.centered, 5.0),
-                  Load(20.0, LoadTypes.centered, 10.0),
-                  Load(1000.0, LoadTypes.centered, 15.0)])
+    # p = Plot(20, [Support(0.0, "fixed"), Support(20.0, "fixed")],
+    #              [Load(15.0, LoadTypes.centered, 5.0),
+    #              Load(20.0, LoadTypes.centered, 10.0),
+    #              Load(1000.0, LoadTypes.centered, 15.0)])
+
+    p = Plot(20.5, [Support(0.0, "fixed")], [
+             Load(15.0, LoadTypes.uniformlyDistributed, 5.0, 10.0)])
     p.plot()
